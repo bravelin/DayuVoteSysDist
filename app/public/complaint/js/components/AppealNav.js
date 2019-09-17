b458647d3c4fc10aaa1aba15fbc415e5facc4112
zhangjialin <lin@yufengtek.com>
2019-09-17 08:57:32 +0800

    2

--- app/public/complaint/js/components/AppealNav.js
+++ app/public/complaint/js/components/AppealNav.js
@@ -1,7 +1,7 @@
 // 底部投诉组件  start
 var AppealNav = Vue.extend({
     template: "<p class='appeal-nav small'><router-link class='deepBlue'" +
-        "to='/appealDetail'>投诉须知</router-link></p>",
+        "to='/appealDetail'></router-link></p>",
 })
 
 Vue.component(AppealNav)
