324fa5d6740c87f81bed28c6f0addd62e066e94f
zhangjialin <lin@yufengtek.com>
2019-10-28 08:25:46 +0800

    add end act schedule

---
+++ app/schedule/endAct.js
@@ -0,0 +1,56 @@
+/**
+ * 更新活动的状态
+ */
+const Subscription = require('egg').Subscription;
+class UpdateActStatus extends Subscription {
+    static get schedule() {
+        return {
+            type: 'all',
+            cron: '0 0 21 3 * *'
+        }
+    }
+    async subscribe () {
+        console.log('================活动状态定时更新。。。。。。')
+        const res = await this.ctx.model.query('call UpdateActStatus();');
+        console.log('call UpdateActStatus().....', res[0])
+        // const ctx = this.ctx;
+        // // 更新活动状态 状态码为 1 2 3 4的活动
+        // const acts = await ctx.service.activity.queryAllNeedUpdateStatusAct();
+        // // console.log('===============schedule status start================');
+        // let act = null;
+        // let now = (new Date()).getTime();
+        // let newStatus = ''
+        // let currStatus = ''
+        // let signUpStartTime = 0
+        // let voteStartTime = 0
+        // let voteEndTime = 0
+        // for (let i = 0; i < acts.length; i++) {
+        //     act = acts[i];
+        //     signUpStartTime = act.signUpStartTime.getTime();
+        //     voteStartTime = act.voteStartTime.getTime();
+        //     voteEndTime = act.voteEndTime.getTime();
+        //     currStatus = act.status;
+        //     if (now < signUpStartTime) {
+        //         newStatus = '1';
+        //     } else {
+        //         newStatus = '2';
+        //         if (now >= voteStartTime) {
+        //             newStatus = '3';
+        //             if (now >= voteEndTime) {
+        //                 newStatus = '4';
+        //                 if (now >= voteEndTime + TenDayTime) { // 投票结束10天之后,改成关闭状态
+        //                     newStatus = '5'
+        //                 }
+        //             }
+        //         }
+        //     }
+        //     if (newStatus != currStatus) { // 需要更新状态
+        //         // console.log('***update status start', act.id, newStatus)
+        //         await act.update({ status: newStatus });
+        //         // console.log('***update status end', act.id, newStatus)
+        //     }
+        // }
+        // console.log('===============schedule status over================');
+    }
+}
+module.exports = UpdateActStatus;
